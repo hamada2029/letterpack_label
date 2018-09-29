b8ead811577796d135b698d234878debe68b93cd
Hamada, Tomonobu <0000yt@gmail.com>
2018-09-29 14:56:56 +0900

    WebWorkerAPI test 2

--- docs/scripts/pdf_worker.js
+++ docs/scripts/pdf_worker.js
@@ -13,13 +13,13 @@ onmessage = function (ev) {
         if(dt.c > 0){return;}
         // なぜか相対URL使えなかった
         // 更新しそうにないスクリプト
-        importScripts(dt.baseURL + 'script/mplus-2m-regular.js');
-        importScripts(dt.baseURL + 'script/imagedata_plus.js');
-        importScripts(dt.baseURL + 'script/imagedata_light.js');
-        importScripts(dt.baseURL + 'script/pdfmake.min.js');
+        importScripts(dt.baseURL + 'scripts/mplus-2m-regular.js');
+        importScripts(dt.baseURL + 'scripts/imagedata_plus.js');
+        importScripts(dt.baseURL + 'scripts/imagedata_light.js');
+        importScripts(dt.baseURL + 'scripts/pdfmake.min.js');
         // こっちもキャッシュ対策
-        importScripts(dt.baseURL + 'script/position.js' + dt.q);
-        importScripts(dt.baseURL + 'script/defineDocCore.js' + dt.q);
+        importScripts(dt.baseURL + 'scripts/position.js' + dt.q);
+        importScripts(dt.baseURL + 'scripts/defineDocCore.js' + dt.q);
 
         var dd = _defineDoc(dt.ao);
         var pdfDocGenerator = pdfMake.createPdf(dd);
